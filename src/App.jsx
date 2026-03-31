8e522f0e784440dcd01a9ab181237ccf43229ac3
miguelGuarrochena <mikeguarrochena@hotmail.com>
2026-03-31 12:04:48 +0200

    Dice created

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
+import { useState } from "react";
+import Die from "./components/Die";
+
 function App() {
+  const generateAllNewDice = () => {
+    return new Array(10).fill(0).map(() => Math.ceil(Math.random() * 6));
+  };
+  const [dice, setDice] = useState(generateAllNewDice());
+
+  const rollDice = () => {
+    setDice(generateAllNewDice());
+  };
+
+  const diceElements = dice.map((num) => <Die value={num} />);
+
   return (
-    <>
-      <h1>Tenzies</h1>
-    </>
+    <main>
+      <div className="dice-container">{diceElements}</div>
+
+      <button className="roll-dice" onClick={rollDice}>
+        Roll
+      </button>
+    </main>
   );
 }
 
---
+++ src/components/Die.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Die = ({ value }) => {
+  return <button>{value}</button>;
+};
+
+export default Die;
