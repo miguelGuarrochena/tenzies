9c75c427e05d1e39773b4410dd3176d06b885415
miguelGuarrochena <mikeguarrochena@hotmail.com>
2026-04-04 20:04:25 +0200

    Accessibility Improvements

--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
-const Die = ({ value }) => {
-  return <button>{value}</button>;
+const Die = ({ value, isHeld, hold }) => {
+  const styles = {
+    backgroundColor: isHeld ? "#59E391" : "white",
+  };
+
+  return (
+    <button
+      style={styles}
+      onClick={hold}
+      aria-pressed={isHeld}
+      aria-label={`Die with value ${value}, ${isHeld} ? "held" : "not held"`}
+    >
+      {value}
+    </button>
+  );
 };
 
 export default Die;
